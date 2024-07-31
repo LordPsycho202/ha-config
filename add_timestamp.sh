1b7961dd7ea431a771264a4f22989464d15f1764
Rodney Nissen <rnissen@thejiraguy.com>
2024-07-31 13:48:18 -0400

    add last debug

--- add_timestamp.sh
+++ add_timestamp.sh
@@ -7,6 +7,7 @@ font="/config/www/fonts/antonio.ttf"
 
 echo "Timestamp: $timestamp"
 echo "File: $file"
+echo "Font: $font"
 
 ffmpeg -i $file -y -vf "drawtext=fontfile=$font: text='$timestamp': x=(w-tw): y=0: fontcolor=white: box=1: boxcolor=0x00000000@1: fontsize=30" $file
 #ffmpeg -vf "drawtext=fontfile=$font: text=$timestamp: x=(w-tw): y=0: fontcolor=white: box=1: boxcolor=0x00000000@1: fontsize=30" -y -i $file $file
