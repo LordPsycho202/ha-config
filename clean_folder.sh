d560dfc5d061c9a53019ef5d96b0d8b1789e6eb2
Rodney Nissen <rnissen@thejiraguy.com>
2024-09-06 16:11:07 -0400

    a few awk adjustments I have zero faith in

--- clean_folder.sh
+++ clean_folder.sh
@@ -4,4 +4,4 @@ folder="$1"
 filetype="$2"
 max="$3"
 
-find $folder/*$filetype -maxdepth 0 -type f -exec stat -c "%Y %n" {} \; | sort -nr | awk "NR > $max {print $2}" | xargs rm -rf
+find $folder/*$filetype -maxdepth 0 -type f -exec stat -c "%Y %n" {} \; | sort -nr | awk 'NR > '$max' {print $2}' | xargs rm -rf
